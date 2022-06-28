4039ad914abb3ee2b83152e68bd93c3cbb1ef74d
Ngowda94 <Nagraj94>
2022-06-28 22:07:01 +0530

    claculating wages for month

--- src/com/bridgelabz/EmployeeWages.java
+++ src/com/bridgelabz/EmployeeWages.java
@@ -4,9 +4,11 @@ public class EmployeeWages {
     final static int EMP_FULL_TIME = 1;
     final static int EMP_PART_TIME = 2;
     final static int WAGES_PER_HRS = 20;
+    final static int DAYS_PER_MONTH = 20;
     public static void main(String[] args) {
         int empHrs = 0;
         int dailyWages = 0;
+        int totalWages = 0;
         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
         switch (empCheck){
             case EMP_FULL_TIME:
@@ -24,5 +26,7 @@ public class EmployeeWages {
         }
         dailyWages = WAGES_PER_HRS * empHrs;
         System.out.println("employee daily wages is: "+ dailyWages);
+        totalWages = dailyWages * DAYS_PER_MONTH;
+        System.out.println("employee monthly wages is: " + totalWages);
     }
 }
