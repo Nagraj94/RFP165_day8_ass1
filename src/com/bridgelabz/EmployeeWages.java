bf072c42fa1b13c430eb50b3ff1aeac9751bfbab
Ngowda94 <Nagraj94>
2022-06-28 21:56:18 +0530

    added part time and employee wages

--- src/com/bridgelabz/EmployeeWages.java
+++ src/com/bridgelabz/EmployeeWages.java
@@ -1,23 +1,27 @@
 package com.bridgelabz;
 
 public class EmployeeWages {
-    final static int EMP_PRESENT = 1;
+    final static int EMP_FULL_TIME = 1;
+    final static int EMP_PART_TIME = 2;
     final static int WAGES_PER_HRS = 20;
     public static void main(String[] args) {
         int empHrs = 0;
-        int totalWages = 0;
+        int dailyWages = 0;
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (EMP_PRESENT == empCheck )
+        if (EMP_FULL_TIME == empCheck )
         {
-            System.out.println("employee is present");
+            System.out.println("employee is present full time ");
             empHrs = 8;
-        }
-        else
+        } else if (EMP_PART_TIME == empCheck)
+        {
+            System.out.println("employee is present for part time ");
+            empHrs = 4;
+        } else
         {
             System.out.println("employee is absent");
             empHrs = 0;
         }
-        totalWages = WAGES_PER_HRS * empHrs;
-        System.out.println("employee total wages is: "+ totalWages);
+        dailyWages = WAGES_PER_HRS * empHrs;
+        System.out.println("employee daily wages is: "+ dailyWages);
     }
 }
